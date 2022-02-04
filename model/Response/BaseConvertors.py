c2ffa3c384c1119424f8a1b29db47b08ad10dcd3
AppDevIn <teamprojectlive@gmail.com>
2022-02-04 22:23:51 +0800

    Refactor the methods to be in common class and added get me feature 🎨 ✨ ⚡️

---
+++ model/Response/BaseConvertors.py
@@ -0,0 +1,35 @@
+from typing import Any, TypeVar, Type, cast
+
+
+T = TypeVar("T")
+
+
+def from_int(x: Any) -> int:
+    assert isinstance(x, int) and not isinstance(x, bool)
+    return x
+
+
+def from_bool(x: Any) -> bool:
+    assert isinstance(x, bool)
+    return x
+
+
+def from_str(x: Any) -> str:
+    assert isinstance(x, str)
+    return x
+
+
+def to_class(c: Type[T], x: Any) -> dict:
+    assert isinstance(x, c)
+    return cast(Any, x).to_dict()
+
+def from_union(fs, x):
+    for f in fs:
+        try:
+            return f(x)
+        except:
+            pass
+    assert False
+
+
+
