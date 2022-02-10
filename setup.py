eace03ee60abc68366a769370395c69078dcedfc
AppDevIn <teamprojectlive@gmail.com>
2022-02-11 01:21:30 +0800

    Revert the project name ⏪️

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TelegramBot8",
+    name="Telegram-Bot8",
     version="0.1.6",
     author="AppDevIn Sliver",
     author_email=" teamprojectlive@example.com",
