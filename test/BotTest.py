5af304727ed1030f33fe8fc3464a266f237e86c6
AppDevIn <teamprojectlive@gmail.com>
2022-02-13 22:15:48 +0800

    test: Update reponse to command , text and the next update id

--- test/BotTest.py
+++ test/BotTest.py
@@ -7,7 +7,7 @@ class BotTest(unittest.TestCase):
     def setUp(self) -> None:
         self.bot = TeleBot("Token")
 
-    def test_generate_updated(self):
+    def test_generate_updated_for_test(self):
         updates = self.bot._generate_updates({
             "ok": True,
             "result": [
@@ -34,8 +34,136 @@ class BotTest(unittest.TestCase):
                 }
             ]
         })
-        print(updates)
         assert len(updates) == 1
+
+    def test_generate_updated_for_command(self):
+        updates = self.bot._generate_updates({
+            "ok": True,
+            "result": [
+                {
+                    "update_id": 221364578,
+                    "message": {
+                        "message_id": 310,
+                        "from": {
+                            "id": 645812448,
+                            "is_bot": False,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "language_code": "en"
+                        },
+                        "chat": {
+                            "id": 645812448,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "type": "private"
+                        },
+                        "date": 1644754404,
+                        "text": "/hello",
+                        "entities": [
+                            {
+                                "offset": 0,
+                                "length": 6,
+                                "type": "bot_command"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "update_id": 221364579,
+                    "message": {
+                        "message_id": 311,
+                        "from": {
+                            "id": 645812448,
+                            "is_bot": False,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "language_code": "en"
+                        },
+                        "chat": {
+                            "id": 645812448,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "type": "private"
+                        },
+                        "date": 1644754415,
+                        "text": "Ggg"
+                    }
+                }
+            ]
+        })
+        assert len(updates) == 2
+
+    def test_generate_updated_for_a_list_of_result(self):
+        updates = self.bot._generate_updates({
+            "ok": True,
+            "result": [
+                {
+                    "update_id": 221364591,
+                    "message": {
+                        "message_id": 330,
+                        "from": {
+                            "id": 645812448,
+                            "is_bot": False,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "language_code": "en"
+                        },
+                        "chat": {
+                            "id": 645812448,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "type": "private"
+                        },
+                        "date": 1644761500,
+                        "text": "/hello",
+                        "entities": [
+                            {
+                                "offset": 0,
+                                "length": 6,
+                                "type": "bot_command"
+                            }
+                        ]
+                    }
+                }
+            ]
+        })
+        assert len(updates) == 1
+
+    def check_if_the_update_id_is_correct(self):
+        updates = self.bot._generate_updates({
+            "ok": True,
+            "result": [
+                {
+                    "update_id": 221364591,
+                    "message": {
+                        "message_id": 1,
+                        "from": {
+                            "id": 645812448,
+                            "is_bot": False,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "language_code": "en"
+                        },
+                        "chat": {
+                            "id": 645812448,
+                            "first_name": "Jeya",
+                            "username": "jrjeya",
+                            "type": "private"
+                        },
+                        "date": 1644761500,
+                        "text": "/hello",
+                        "entities": [
+                            {
+                                "offset": 0,
+                                "length": 6,
+                                "type": "bot_command"
+                            }
+                        ]
+                    }
+                }
+            ]
+        })
+        assert updates[0].getNextUpdateID() == 2
 
     def test_generate_updated_throw_value_exception(self):
         self.assertRaises(ValueError, self.bot._generate_updates, {
