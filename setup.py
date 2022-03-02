62356bc621b30041a7470f60243be652b757af1d
AppDevIn <teamprojectlive@gmail.com>
2022-03-02 10:21:13 +0800

    refactor: bump the version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Telegram-Bot8",
-    version="0.1.6",
+    version="0.2.0",
     author="AppDevIn Sliver",
     author_email=" teamprojectlive@example.com",
     description="Python package to utilizes the telegram API to easily add commands and person other actions",
