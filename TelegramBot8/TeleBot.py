d775dd0aa4c96d11b5b8c57eeb535eb523d07601
AppDevIn <teamprojectlive@gmail.com>
2022-04-15 10:15:36 +0800

    fix: Add header to the request and convert into json for the payload

--- TelegramBot8/TeleBot.py
+++ TelegramBot8/TeleBot.py
@@ -225,7 +225,7 @@ class TeleBot:
             .reply_to_message_id(reply_to_message_id) \
             .allow_sending_without_reply(allow_sending_without_reply) \
             .reply_markup(reply_markup).build()
-        requests.request("POST", url, headers={'Content-Type': 'application/json'}, data=json.dumps(request_body))
+        requests.request("POST", url, headers=self.headers, data=json.dumps(request_body))
 
     def forward_messaged(self, chat_id, from_chat_id, message_id: int,
                          disable_notification: bool = None, protect_content: bool = None) -> ForwardResponse:
@@ -246,7 +246,7 @@ class TeleBot:
         request_body = request_body.chat_id(chat_id).from_chat_id(from_chat_id).message_id(message_id). \
             disable_notification(disable_notification).protect_content(protect_content).build()
 
-        response = requests.post(url, headers={}, data=request_body)
+        response = requests.post(url, headers=self.headers, data=json.dumps(request_body))
         return forward_from_dict(response.text)
 
     def set_my_commands(self, commands: [BotCommand], scope: {} = None, language_code: str = None) -> BaseResponse:
